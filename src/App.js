55030a46cd05827078637a16b5dff9b85b627fc5
IBNSHADIQ\ardanclassic <akbar@glovory.com>
2021-06-20 11:09:50 +0700

    add some assets file & update scripts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import Sidebar from './components/sidebar/index';
+import { Dashboard, DefaultPage } from './components/contents'
+import { FileData } from 'assets/data'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
+  let menus = [];
+  JSON.stringify(FileData, (key, value) => {
+    if (key === 'id') menus.push(value);
+    return value;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Provider store={ store }>
+      <div className="container">
+        <Router>
+          <div className="columns">
+            <Sidebar className="column" />
+            <div className="column main-content">
+              <Switch>
+                <Route exact path="/" component={ Dashboard } />
+                <Route path="/dashboard" component={ Dashboard } />
+                { menus.map(el => {
+                  return <Route key={el} path={`/${el}`} component={ DefaultPage } />
+                }) }
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
